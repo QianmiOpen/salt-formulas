06184ce157f7bd17e2aed860a1bfc05872597bf1
彭毅 <pengyi@ofcard.com>
2014-12-01 20:18:55 +0800

    修改退出条件

--- webapp/files/dubbo_weight_jmx.py
+++ webapp/files/dubbo_weight_jmx.py
@@ -23,7 +23,7 @@ class cmd(object):
         if (ret):
             executeTime = 0
             while (executeTime < maxExecuteTimes):
-                if (invokeNum <= self.maxInvoke):
+                if (invokeNum < self.maxInvoke):
                     return True
                 time.sleep(self.interval)
                 (ret, invokeNum) = self.cmdExecute()
