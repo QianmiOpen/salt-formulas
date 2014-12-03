2f01608407f8f69fe6fd52e497534f2fe20eedf3
mind <mind@peace.local>
2014-12-03 18:26:29 +0800

    增加优雅启动和停止功能

--- webapp/files/dubbo_weight_jmx.py
+++ webapp/files/dubbo_weight_jmx.py
@@ -1,129 +1,123 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# from requests.auth import HTTPBasicAuth
-# import requests
-import time
 import socket
-import os
 import sys
-import sched, time
 import commands
 import urllib
-
+import time
 
 class cmd(object):
-    def __init__(self, maxInvoke, interval, timeout):
-        self.maxInvoke = maxInvoke
-        self.interval = interval
-        self.timeout = timeout
-
-    def cmdInterval(self):
-        maxExecuteTimes = (self.timeout + self.interval -1) / self.interval
-        (ret, invokeNum) = self.cmdExecute()
-        print("execute result %r, %d. max execute %d" % (ret, invokeNum, maxExecuteTimes))
+    def check_app_start(self, interval, timeout):
+        jmx_name = "com.ofpay.health:name=HealthStatus"
+        jxm_attr = "Started"
+        max_execute_times = (timeout + interval - 1) / interval
+        (ret, started) = self.get_jmx(jmx_name, jxm_attr, lambda x: "true" in x.lower(), False)
+        print("execute result %r, %r. max execute %d." % (ret, started, max_execute_times))
         if (ret):
-            executeTime = 0
-            while (executeTime < maxExecuteTimes):
-                if (invokeNum < self.maxInvoke):
+            execute_time = 0
+            while (execute_time < max_execute_times):
+                if (started):
                     return True
-                time.sleep(self.interval)
-                (ret, invokeNum) = self.cmdExecute()
+                time.sleep(interval)
+                (ret, started) = self.get_jmx(jmx_name, jxm_attr, lambda x: "true" in x.lower(), False)
                 if (not ret):
                     return False
-                executeTime += 1
-                print("wait %d times, invoke num %d" % (executeTime, invokeNum))
+                execute_time += 1
+                print("wait %d times, is started: %r" % (execute_time, started))
 
-            print("dubbo thread num is %d" % invokeNum)
             return False
         else:
-            time.sleep(self.timeout)
+            time.sleep(timeout)
             return True
 
-    def cmdExecute(self):
+    def check_dubbo_stop(self, max_invoke, interval, timeout):
+        jmx_name = "com.qianmi:name=DubboProviderMBean"
+        jxm_attr = "AliveEvents"
+        max_execute_times = (timeout + interval - 1) / interval
+        (ret, invoke_num) = self.get_jmx(jmx_name, jxm_attr, int, 0)
+        print("execute result %r, %d. max execute %d" % (ret, invoke_num, max_execute_times))
+        if (ret):
+            execute_time = 0
+            while (execute_time < max_execute_times):
+                if (invoke_num < max_invoke):
+                    return True
+                time.sleep(interval)
+                (ret, invoke_num) = self.get_jmx(jmx_name, jxm_attr, int, 0)
+                if (not ret):
+                    return False
+                execute_time += 1
+                print("wait %d times, invoke num %d" % (execute_time, invoke_num))
+
+            return False
+        else:
+            time.sleep(timeout)
+            return True
+
+    def get_jmx(self, name, attr, retTrans, default = "0"):
         try:
-            (eventStatus, eventResult) = commands.getstatusoutput('java -jar /home/tomcat/cmdline-jmxclient-0.10.3.jar - 127.0.0.1:9000 com.qianmi:name=DubboInvokeMBean AliveProviderEvents')
-            eventStatus >>= 8
-            eventResult = eventResult[eventResult.rfind(':') + 1:]
-            if(eventStatus != 0):
-                return (False, 0)
+            (event_status, event_result) = commands.getstatusoutput("java -jar /home/tomcat/cmdline-jmxclient-0.10.3.jar - 127.0.0.1:9000 %s %s" % (name, attr))
+            print "jmx result: %d, %s" % (event_status, event_result)
+            event_status >>= 8
+            event_result = event_result[event_result.rfind(':') + 1:]
+            if(event_status != 0):
+                return (False, default)
             else:
                 try:
-                    return (True, int(eventResult))
+                    return (True, retTrans(event_result))
                 except:
                     print "except error: DubboInvokeMBean is not a registered bean"
-                    return (False, 0)
+                    return (False, default)
         except:
             print "Unexpected error:", sys.exc_info()[0]
-            return (False, 0)
+            return (False, default)
 
-#dubbo服务
 class dubbo(object):
-    def __init__(self, monitor, user, pwd, provider, weight):
-        self.monitor = monitor
-        self.user = user
-        self.pwd = pwd
-        self.provider = provider
-        self.weight = str(weight)
+    SUCCESS = u'操作成功'
+    NO_PROVIDER = 'there is no provider'
 
-        self.boolean_true = u'操作成功'
-        self.boolean_false = 'dubbo execute false'
-        self.boolean_noProvider = 'there is no provider'
-
-    def operatorFun(self):
-        opeUrl = 'http://'+self.user+':'+self.pwd+'@'+self.monitor+'/governance/addresses/'+self.provider+'/providers/weight?weight=' + self.weight
+    def set_dubbo_weight(self, monitor, user, pwd, provider, weight):
+        opeUrl = "http://%s:%s@%s/governance/addresses/%s:/providers/weight?weight=%s" % (user, pwd, monitor, provider, weight)
         print opeUrl
         try:
             r_ope = urllib.urlopen(opeUrl)
             text = r_ope.read().decode('utf-8')
-            # print text
-
-            int_result = text.find(self.boolean_true)
-            # print int_result
-            if int_result == -1:
-                if(text.find(self.boolean_noProvider) != -1):
-                    print self.boolean_noProvider
-                    # raise Exception(self.boolean_noProvider)
+            if text.find(dubbo.SUCCESS) == -1:
+                if(text.find(dubbo.NO_PROVIDER) != -1):
+                    print dubbo.NO_PROVIDER
+                    return False
                 else:
-                    raise Exception(self.boolean_false)
+                    print text
+                    return False
             else :
-                print self.boolean_true.encode('utf-8')
+                print dubbo.SUCCESS.encode('utf-8')
                 return True
         except:
             print "Unexpected error:", sys.exc_info()[0]
             raise
 
 if __name__ == "__main__":
-    if(len(sys.argv) != 5):
-        errorStr = "您需要传递4个参数，格式为：python dubbo_weight.py dubboadmin:port adminUser adminPassword weight;\n 1、dubboadmin:port(dubboadmin应用暴露的服务),2、adminUser(dubboadmin登录用户),3、adminPassword(dubboadmin登录密码),4、weight(权重)"
-        print errorStr
-        raise Exception(errorStr)
+    if(len(sys.argv) != 6):
+        error_str = "您需要传递5个参数，格式为：python dubbo_weight.py up|down dubboadmin:port adminUser adminPassword weight;\n 1、操作动作 \n 2、dubboadmin:port(dubboadmin应用暴露的服务) \n 3、adminUser(dubboadmin登录用户) \n 4、adminPassword(dubboadmin登录密码) \n 5、weight(权重)"
+        print error_str
+        raise Exception(error_str)
     else:
-        print(sys.argv[0])
-        dubboadmin = sys.argv[1]
-        print(dubboadmin)
-        adminUser = sys.argv[2]
-        print(adminUser)
-        adminPassword = sys.argv[3]
-        print(adminPassword)
-        weight = sys.argv[4]
-        print(weight)
+        operate, dubbo_admin, admin_user, admin_password, weight = sys.argv[1:6]
 
-        myname = socket.getfqdn(socket.gethostname())
-        print(myname)
-        myaddr = socket.gethostbyname(myname)
-        print(myaddr)
+        my_name = socket.getfqdn(socket.gethostname())
+        my_addr = socket.gethostbyname(my_name)
+        print(my_addr)
 
-        # dubboadmin = "172.19.65.13:8080"
-        # adminUser = "root"
-        # adminPassword = "master123"    
-        # myaddr = "172.19.65.22"
-        # weight = 0
         try:
-            dubbo(dubboadmin, adminUser, adminPassword, myaddr+':', weight).operatorFun()
-            if(int(weight) == 0):
-                if not cmd(1, 3, 30).cmdInterval():
+            if (operate.lower() == "up"):
+                if not cmd().check_app_start(3, 180):
+                    raise Exception("cmd execute exception")
+                if not dubbo().set_dubbo_weight(dubbo_admin, admin_user, admin_password, my_addr, weight):
+                    raise Exception("stop dubbo exception")
+            else:
+                if not dubbo().set_dubbo_weight(dubbo_admin, admin_user, admin_password, my_addr, weight):
+                    raise Exception("stop dubbo exception")
+                if not cmd().check_dubbo_stop(1, 3, 30):
                     raise Exception("cmd execute exception")
         except:
             print "Unexpected error:", sys.exc_info()[0]
             raise
-        # return cmdResult
